d3e2b3388b0b69d9a533360d2f3f372f4aa8b5bf
Caleb <kamenjuhkiiru@gmail.com>
2023-11-27 20:51:30 +0300

    Count sentences not done yet

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,28 @@
-#!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+    def __init__(self, value="This is the default value."):
+        self._value = value
+
+    def get_value(self):
+        return self._value
+
+    def set_value(self, new_value):
+        if type(new_value) == str:
+            self._value = new_value
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        return self._value.endswith('.')
+
+    def is_question(self):
+        return self._value.endswith('?')
+
+    def is_exclamation(self):
+        return self._value.endswith('!')
+
+    def count_sentences(self):
+        pass
+
+    new_value = property(get_value, set_value)
